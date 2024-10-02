2ab78a9c1155929ffb600f4006d2acb0c4762016
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-10-02 22:22:40 +0300

    Add project files.

---
+++ week-1/FirstApp/FirstApp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace week1
+{
+    class FirstApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mahir Özkan");
+            Console.WriteLine("Kendimi yazılım alanında geliştirmek ve keşkelerimden kurtulmak için bu bootcampteyim!");
+        }
+    }
+}
