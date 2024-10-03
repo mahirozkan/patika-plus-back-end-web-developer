ba3626524f4cc909e09a023ca330916352459be5
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-10-03 18:56:37 +0300

    If-Else ifadeleri kullanılarak girilen sayının 10'dan büyük, küçük veya 10'a eşit olduğunu, ayrıca sayının çift ya da tek olduğunu belirten program oluşturuldu.

---
+++ week-1/ElseIf/ElseIf.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ElseIfExample
+{
+    class ElseIf
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir sayi giriniz:");
+
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi > 10)
+            {
+                Console.WriteLine("Girilen sayi 10'dan büyüktür.");
+
+            }
+            else if (sayi < 10)
+            {
+                Console.WriteLine("Girilen sayi 10'dan kücüktür.");
+                
+            }
+            else
+            {
+                Console.WriteLine("Girilen sayi 10'a esittir.");
+
+            }
+
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("Girilen sayi cifttir.");
+            }
+            else
+            {
+                Console.WriteLine("Girilen sayi tektir.");
+            }
+        }
+    }
+}
