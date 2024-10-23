a94f12511b37b89b30920aa2d42cedaedf73e4c0
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-10-23 22:40:04 +0300

    Hafta4 Ödev1 oluşturuldu

---
+++ week-4/IlkSinif/IlkSinif/IlkSinif/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlkSinif
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string Role {  get; set; }
+
+        public void GetDetails()
+        {
+            Console.WriteLine($"{Name} {SurName} isimli {Role}, {BirthDate} tarihinde doğmuştur.");
+        }
+
+    }
+}
---
+++ week-4/IlkSinif/IlkSinif/IlkSinif/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace IlkSinif
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ogrenci1 = new Person();
+            ogrenci1.Name = "Mahir";
+            ogrenci1.SurName = "Özkan";
+            ogrenci1.BirthDate = new DateOnly(1998,07,05);
+            ogrenci1.Role = "öğrenci";
+
+            Person ogrenci2 = new Person();
+            ogrenci2.Name = "Selim";
+            ogrenci2.SurName = "Yılmaz";
+            ogrenci2.BirthDate = new DateOnly(1992, 09, 22);
+            ogrenci2.Role = "öğrenci";
+
+            Person ogrenci3 = new Person();
+            ogrenci3.Name = "Ayşe";
+            ogrenci3.SurName = "Çelik";
+            ogrenci3.BirthDate = new DateOnly(1995, 04, 18);
+            ogrenci3.Role = "öğrenci";
+
+            Person ogrenci4 = new Person();
+            ogrenci4.Name = "Derya";
+            ogrenci4.SurName = "Aydın";
+            ogrenci4.BirthDate = new DateOnly(1997, 02, 12);
+            ogrenci4.Role = "öğrenci";
+
+            Person ogretmen1 = new Person();
+            ogretmen1.Name = "Zeynep";
+            ogretmen1.SurName = "Şahin";
+            ogretmen1.BirthDate = new DateOnly(1987, 08, 30);
+            ogretmen1.Role = "öğretmen";
+
+            Person ogretmen2 = new Person();
+            ogretmen2.Name = "Emre";
+            ogretmen2.SurName = "Kılıç";
+            ogretmen2.BirthDate = new DateOnly(1988, 01, 14);
+            ogretmen2.Role = "öğretmen";
+
+            ogrenci1.GetDetails();
+            ogrenci2.GetDetails();
+            ogrenci3.GetDetails();
+            ogrenci4.GetDetails();
+            ogretmen1.GetDetails();
+            ogretmen2.GetDetails();
+
+            Console.ReadKey();
+        }
+    }
+}
