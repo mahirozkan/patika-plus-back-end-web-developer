4bd79bb43249b386155df7f289e9111b00765fc4
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-10-24 22:39:16 +0300

    Proje dosyaları oluşturuldu.

---
+++ week-4/Constructor/Constructor/Bebek.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Constructor
+{
+    internal class Bebek
+    {
+        // Bebek sınıfı için isim, soyisim ve doğum tarihi alanlarını tanımlıyoruz
+        public string _name;
+        public string _surname;
+        public DateTime _birthDate;
+
+        // Varsayılan constructor (yapıcı metot)
+        public Bebek() 
+        {
+            // Eğer bebek adı ya da soyadı verilmezse, "Bilinmiyor" olarak ayarlanıyor
+            _birthDate = DateTime.Now; // Doğum tarihi şu anki zaman olarak ayarlanıyor
+            _name = "Bilinmiyor";
+            _surname = "Bilinmiyor";
+        }
+
+        // Parametreli constructor (isim ve soyisim verildiğinde çalışır)
+        public Bebek(string name, string surname)
+        {
+            // Bebek ismi ve soyismi dışarıdan gelen parametrelerle ayarlanıyor
+            _name = name;
+            _surname = surname;
+            _birthDate = DateTime.Now;
+        }
+
+        // Bebek doğumunu ekrana yazdıran metot
+        public void BebekDogum()
+        {
+            Console.WriteLine($"{_name} {_surname} bebek, {_birthDate} tarihinde doğdu.");
+            Console.WriteLine("Ingaaaa!!");
+        }
+    }
+}
---
+++ week-4/Constructor/Constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Varsayılan constructor ile bir Bebek nesnesi oluşturuyoruz
+            Bebek bebek1 = new Bebek();
+            bebek1._name = "Mahir";
+            bebek1._surname = "Özkan";
+            bebek1.BebekDogum();
+
+            // Parametreli constructor kullanarak bir Bebek nesnesi oluşturuyoruz
+            Bebek bebek2 = new Bebek("Melek", "Özkan");
+            bebek2.BebekDogum();
+
+            Console.ReadLine();
+        }
+    }
+}
