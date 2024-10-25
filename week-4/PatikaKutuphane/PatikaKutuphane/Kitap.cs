51588dc3c151dd7d64c1c9017f18f6ac97a4dd60
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-10-25 19:07:10 +0300

    Proje dosyaları oluşturuldu.

---
+++ week-4/PatikaKutuphane/PatikaKutuphane/Kitap.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaKutuphane
+{
+    internal class Kitap  // Kitap sınıfı (Class): Kitap özelliklerini ve davranışlarını tanımlar
+    {
+        // Özellikler (Properties): Kitabın özelliklerini tutan alanlar
+        public string _kitapAdi;
+        public string _yazarAdi;
+        public string _yazarSoyadi;
+        public int _sayfaSayisi;
+        public string _yayinevi;
+        public DateOnly _kayitTarihi;
+
+        // Parametresiz Constructor: Nesne oluşturulurken varsayılan değerler atar
+        public Kitap()
+        {
+            _kitapAdi = "Bilinmiyor";
+            _yazarAdi = "Bilinmiyor";
+            _yazarSoyadi = "Bilinmiyor";
+            _sayfaSayisi = 0;
+            _yayinevi = "Bilinmiyor";
+            _kayitTarihi = DateOnly.FromDateTime(DateTime.Now);
+        }
+
+        // Parametre alan Constructor: Kitap bilgilerini parametre olarak alır ve özelliklere atar
+        public Kitap(string kitapAdi, string yazarAdi, string yazarSoyadi, int sayfaSayisi, string yayinevi)
+        {
+            _kitapAdi = kitapAdi;
+            _yazarAdi = yazarAdi;
+            _yazarSoyadi = yazarSoyadi;
+            _sayfaSayisi = sayfaSayisi;
+            _yayinevi = yayinevi;
+            _kayitTarihi= DateOnly.FromDateTime(DateTime.Now);
+        }
+
+        // Kitap bilgilerini yazdıran metot
+        public void KitapBilgileri()
+        {
+            Console.WriteLine($"Kitap Adı: {_kitapAdi}\r\n" +
+                $"Yazar: {_yazarAdi} {_yazarSoyadi}\r\n" +
+                $"Sayfa Sayısı: {_sayfaSayisi}\r\n" +
+                $"Yayınevi: {_yayinevi}\r\n" +
+                $"Kayıt Tarihi: {_kayitTarihi.ToString("yyyy-MM-dd")}\r\n");
+        }
+    }
+}
---
+++ week-4/PatikaKutuphane/PatikaKutuphane/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace PatikaKutuphane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Kitap kitap1 = new Kitap(); // new anahtar kelimesi ile Kitap sınıfından bir nesne oluşturuyoruz (kitap1), parametresiz constructor kullanılır
+            kitap1._kitapAdi = "Bir Kitap"; 
+            kitap1._yazarAdi = "Ahmet";
+            kitap1._yazarSoyadi = "Yılmaz";
+            kitap1._sayfaSayisi = 320;
+            kitap1._yayinevi= "Alfa Yayınları";
+            kitap1._kayitTarihi=new DateOnly(2023,05,14);
+
+        Kitap kitap2 = new Kitap();
+            kitap2._kitapAdi = "Başka Bir Kitap";
+            kitap2._yazarAdi = "Ayşe";
+            kitap2._yazarSoyadi = "Karan";
+            kitap2._sayfaSayisi = 215;
+            kitap2._yayinevi = "Beta Yayıncılık";
+            kitap2._kayitTarihi = new DateOnly(2023, 06, 20);
+
+        Kitap kitap3 = new Kitap();
+            kitap3._kitapAdi = "Daha Başka Kitap";
+            kitap3._yazarAdi = "Mehmet";
+            kitap3._yazarSoyadi = "Demir";
+            kitap3._sayfaSayisi = 450;
+            kitap3._yayinevi = "Gamma Yayınları";
+            kitap3._kayitTarihi = new DateOnly(2023, 07, 01);
+
+        Kitap kitap4 = new Kitap("Son Kitap", "Elif", "Şahin", 275, "Delta Yayıncılık"); // new anahtar kelimesi ile Kitap sınıfından parametreli bir nesne oluşturuyoruz (kitap4)
+            Kitap kitap5 = new Kitap("Örnek Kitap", "Selin", "Yurt", 198, "Epsilon Yayınları");
+
+            kitap1.KitapBilgileri();
+            kitap2.KitapBilgileri();
+            kitap3.KitapBilgileri();
+            kitap4.KitapBilgileri();
+            kitap5.KitapBilgileri();
+            Console.ReadLine();
+        }
+    }
+}
