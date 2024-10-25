2f112b430b52407648fe6db84dab9ad340dba07b
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-10-26 00:01:00 +0300

    Proje dosyalari olu;turuldu/

---
+++ week-4/Encapsulation/Encapsulation/Araba.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Araba
+    {
+        public string marka {  get; set; } // Arabanın markasını tutacak özellik
+        public string model { get; set; } // Arabanın modelini tutacak özellik
+        public string renk { get; set; } // Arabanın rengini tutacak özellik
+        private int kapiSayisi; // Kapı sayısı yalnızca sınıf içinde erişilebilir, doğrudan atanamıyor
+
+        public int KapiSayisi // Kapı sayısına dışarıdan erişimi sağlayan özellik
+        {
+            get { return kapiSayisi; }
+            set
+            {
+                if (value == 2 || value == 4)  // Kapı sayısı 2 veya 4 olduğunda geçerli bir değer olarak kabul ediyoruz
+                {
+                    kapiSayisi = value;
+                }
+                else
+                {
+                    Console.WriteLine("Yanlış bir kapı sayısı girdiniz!"); // Geçersiz bir değer girildiğinde uyarı verip varsayılan bir değer atıyoruz
+                    kapiSayisi = -1;
+                }
+                
+            }
+        }
+
+        public void ArabaBilgisi() // Arabanın bilgilerini ekrana yazdıran metod
+        {
+            Console.WriteLine($"Marka: {marka}\r\n" +
+                $"Model: {model}\r\n" +
+                $"Renk: {renk}\r\n" +
+                $"Kapı Sayısı: {kapiSayisi}");
+        }
+    }
+}
+
---
+++ week-4/Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba araba = new Araba(); // Yeni bir Araba nesnesi oluşturuyoruz
+            araba.marka = "Ford"; // Arabanın marka, model ve renk özelliklerine değerler atıyoruz
+            araba.model = "Mustang";
+            araba.renk = "Siyah";
+            araba.KapiSayisi = 2; // Kapı sayısını 2 olarak atıyoruz (geçerli bir değer)
+
+            araba.ArabaBilgisi(); // Araba bilgilerini ekrana yazdırıyoruz
+            Console.ReadLine();
+        }
+    }
+}
