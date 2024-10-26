38e60230092bdc8bd9dfacbb5c9afe5f9fb4315f
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-10-26 12:39:31 +0300

    Proje dosyaları oluşturuldu.

---
+++ week-4/Inheritance/Inheritance/Base.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Base // Base sınıf; tüm bireylerde ortak olacak özellikleri barındırır
+    {
+        // Her birey için isim ve soyisim özellikleri
+        public string ad { get; set; }
+        public string soyad { get; set; }
+        public void Info() // Bireyin temel bilgilerini ekrana yazdıran metot
+        {
+            Console.WriteLine($"Ad: {ad}\r\n" +
+                $"Soyad: {soyad}");
+        }
+    }
+    public class Ogrenci : Base // Öğrenci sınıfı, Base sınıfından miras alır
+    {
+        public int ogrenciNumarasi { get; set; } // Öğrenciye özel öğrenci numarası özelliği
+
+        public void OgrenciInfo()  // Öğrencinin tüm bilgilerini ekrana yazdıran metot
+        {
+            Console.WriteLine($"Ad: {ad}\r\n" +
+            $"Soyad: {soyad}\r\n" +
+            $"Öğrenci Numarası: {ogrenciNumarasi}");
+        }
+    }
+    public class Ogretmen : Base // Öğretmen sınıfı, Base sınıfından miras alır
+    {
+        public int maas { get; set; } // Öğretmene özel maaş özelliği
+
+        public void OgretmenInfo() // Öğretmenin tüm bilgilerini ekrana yazdıran metot
+        {
+            Console.WriteLine($"Ad: {ad}\r\n" +
+            $"Soyad: {soyad}\r\n" +
+            $"Maaş: {maas} TL");
+        }
+    }
+}
+
---
+++ week-4/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Yeni bir öğrenci nesnesi oluşturuyoruz ve özelliklerini atıyoruz
+            Ogrenci ogrenci = new Ogrenci();
+            ogrenci.ad = "Mahir";
+            ogrenci.soyad = "Özkan";
+            ogrenci.ogrenciNumarasi = 1171601020;
+
+            // Yeni bir öğretmen nesnesi oluşturuyoruz ve özelliklerini atıyoruz
+            Ogretmen ogretmen = new Ogretmen();
+            ogretmen.ad = "Mesut";
+            ogretmen.soyad = "Mike";
+            ogretmen.maas = 45000;
+
+            ogrenci.OgrenciInfo(); // Öğrencinin bilgilerini ekrana yazdırıyoruz
+            ogretmen.OgretmenInfo(); // Öğretmenin bilgilerini ekrana yazdırıyoruz
+
+            Console.ReadLine();
+        }
+    }
+}
