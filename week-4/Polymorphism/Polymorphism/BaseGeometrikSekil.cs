ad14d1cd72fc268d97da019550d65830494279f8
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-10-27 14:30:23 +0300

    Proje dosyaları oluşturuldu.

---
+++ week-4/Polymorphism/Polymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public abstract class BaseGeometrikSekil // Tüm geometrik şekiller için temel soyut sınıf
+    {
+        public virtual string Ad { get { return "Şekil"; } } // Şeklin adı (varsayılan olarak "Şekil")
+
+        // Şeklin genişlik ve yükseklik özellikleri
+        public int Genislik { get; set; }
+        public int Yukseklik { get; set; }
+        public abstract double Alan(); // Alan hesaplama metodu - her türetilmiş sınıf kendine göre tanımlayacak
+        public override string ToString() // Şekli ve boyutlarını döndüren metin temsilini sağlar
+        {
+            return $"{Ad} ({Genislik}x{Yukseklik})";
+        }
+    }
+    public class Kare : BaseGeometrikSekil // Kare sınıfı - BaseGeometrikSekil'den türetilmiştir
+    {
+        public override string Ad => "Kare"; // Ad özelliği, "Kare" olarak değiştirildi
+        public override double Alan() // Alanı hesaplayan metod, genişlik ve yüksekliğin çarpımı olarak tanımlandı
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+    public class Dikdortgen : BaseGeometrikSekil // Dikdörtgen sınıfı - BaseGeometrikSekil'den türetilmiştir
+    {
+        public override string Ad => "Dikdortgen"; // Ad özelliği, "Dikdörtgen" olarak değiştirildi
+        public override double Alan() // Alanı hesaplayan metod, genişlik ve yüksekliğin çarpımı olarak tanımlandı
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+    public class DikUcgen : BaseGeometrikSekil // Dik Üçgen sınıfı - BaseGeometrikSekil'den türetilmiştir
+    {
+        public override string Ad => "DikUcgen"; // Ad özelliği, "Dik Üçgen" olarak değiştirildi
+        public override double Alan()  // Alanı hesaplayan metod, genişlik ve yüksekliğin çarpımının yarısı olarak tanımlandı
+        {
+            return (Genislik * Yukseklik) / 2;
+        }
+    }
+}
---
+++ week-4/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kare, Dikdörtgen ve Dik Üçgen nesneleri oluşturuluyor
+            BaseGeometrikSekil sekil1 = new Kare() { Genislik = 10, Yukseklik = 10 };
+            BaseGeometrikSekil sekil2 = new Dikdortgen() { Genislik = 10, Yukseklik = 20 };
+            BaseGeometrikSekil sekil3 = new DikUcgen() { Genislik = 10, Yukseklik = 10 };
+
+            // Her bir şeklin ToString metodu çağırılarak şekil bilgileri yazdırılıyor
+            Console.WriteLine(sekil1);
+            Console.WriteLine($"Alanı: {sekil1.Alan()}");
+            Console.WriteLine(sekil2);
+            Console.WriteLine($"Alanı: {sekil2.Alan()}");
+            Console.WriteLine(sekil3);
+            Console.WriteLine($"Alanı: {sekil3.Alan()}");
+
+            Console.ReadLine();
+        }
+    }
+}
