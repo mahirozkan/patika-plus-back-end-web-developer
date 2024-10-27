cac60de9c431189d38c54c525bc1030b3dce1998
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-10-27 22:10:47 +0300

    Proje dosyaları oluşturuldu.

---
+++ week-4-bomb/week-4-bomb/week-4-bomb/BaseMakine.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_4_bomb
+{
+    public abstract class BaseMakine
+    {
+        public DateTime uretimTarihi => DateTime.Now;
+        public int seriNo;
+        public string ad;
+        public string aciklama;
+        public string isletimSistemi;
+
+        public virtual void GetInfo()
+        {
+            Console.WriteLine($"Üretim Tarihi: {uretimTarihi}\r\n" +
+                $"Seri Numarası: {seriNo}\r\n" +
+                $"Ad: {ad}\r\n" +
+                $"Açıklama: {aciklama}\r\n" +
+                $"İşletim Sistemi: {isletimSistemi}");
+        }
+    }
+
+    public class Telefon : BaseMakine
+    {
+        private string _lisans;
+        public string lisans
+        {
+            get => _lisans;
+            set
+            {
+                if (value == "evet" || value == "hayır")
+                {
+                    _lisans = value;
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı bir giriş yaptınız!");
+                    _lisans = "Lisans yok";
+                }
+            }
+        }
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"Tr Lisans: {_lisans}");
+            Console.WriteLine($"Telefon başarıyla üretildi.");
+        }
+    }
+
+    public class Bilgisayar : BaseMakine
+    {
+        private int _usbSayisi;
+        public int usbSayisi
+        {
+            get => _usbSayisi;
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    _usbSayisi = value;
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı seçim yaptınız!");
+                    _usbSayisi = -1;
+                }
+            }
+        }
+        private string _bluetooth;
+        public string bluetooth
+        {
+            get => _bluetooth;
+            set
+            {
+                if (value == "var" || value== "yok")
+                {
+                    _bluetooth = $"Bluetooth {value}";
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı bir giriş yaptınız!");
+                    _bluetooth = "Bluetooth yok";
+                }
+            }
+        }
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"USB Girişi Sayısı: {_usbSayisi}");
+            Console.WriteLine($"Bluetooth: {_bluetooth}");
+            Console.WriteLine("Bilgisayar başarıyla üretildi.");
+        }
+
+    }
+}
---
+++ week-4-bomb/week-4-bomb/week-4-bomb/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace week_4_bomb
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        Secim1:
+            Console.Write("Telefon üretmek için '1'e, Bilgisayar üretmek için '2'ye basınız.");
+            int secim;
+            while (!int.TryParse(Console.ReadLine(), out secim) || (secim != 1 && secim != 2))
+            {
+                Console.Write("Lütfen geçerli bir seçim yapın (1 veya 2): ");
+            }
+
+            switch (secim)
+            {
+                case 1:
+                    {
+                        Telefon telefon = new Telefon();
+                        Console.Write("Seçiminiz Telefon.\r\n");
+                        Console.Write("Tr Lisansı var mı?(evet/hayır):");
+                        string _lisans = Console.ReadLine();
+                        telefon.lisans = _lisans;
+                        telefon.GetInfo();
+                        break;
+                    }
+                case 2:
+                    {
+                        Bilgisayar bilgisayar = new Bilgisayar();
+                        Console.Write("Seçiminiz Bilgisayar.\r\n");
+                        Console.Write("USB girişi sayısı?(2/4): \r\n");
+                        int _usbSayisi;
+                        while (!int.TryParse(Console.ReadLine(), out _usbSayisi) || (_usbSayisi != 2 && _usbSayisi != 4))
+                        {
+                            Console.Write("Lütfen geçerli bir seçim yapın (2 veya 4): ");
+                        }
+                        bilgisayar.usbSayisi = _usbSayisi;
+                        Console.Write("Bluetooth var mı?(var/yok): ");
+                        string _bluetooth = Console.ReadLine();
+                        bilgisayar.bluetooth = _bluetooth;
+                        bilgisayar.GetInfo();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Hatalı bir seçim yaptınız!Tekrar seçiniz.");
+                        goto Secim1;
+                    }
+            }
+
+            Console.Write("Başka bir ürün üretmek istiyor musunuz?(evet/hayır): ");
+
+            string secim2 = Console.ReadLine(); 
+
+            switch (secim2)
+            {
+                case "evet":
+                    Console.Clear();
+                    goto Secim1;
+                default:
+                    Console.WriteLine("Çıkış yapılıyor.");
+                    return; 
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
