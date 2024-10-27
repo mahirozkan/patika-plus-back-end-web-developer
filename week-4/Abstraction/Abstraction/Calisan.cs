2410d3a0d2b4b91cb542f6c3ebaeecc6f88e75cb
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-10-27 19:44:56 +0300

    Proje dosyaları oluşturuldu.

---
+++ week-4/Abstraction/Abstraction/Calisan.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class Calisan // 'Calisan' sınıfı, tüm çalışanlar için ortak özellikleri ve yöntemleri barındırır.
+    {
+        public string name;
+        public string surname;
+        public string department;
+
+        public abstract void GetInfo(); // Alt sınıfların kendine özgü bilgilerini göstereceği soyut bir metod.
+    }
+    public class ProjeYoneticisi : Calisan // Proje yöneticisi sınıfı, 'Calisan' sınıfından türetiliyor.
+    {
+        public override void GetInfo()  // 'GetInfo' metodunu proje yöneticisine özel bir mesajla uyguluyor.
+        {
+            Console.WriteLine($"Ad: {name}\r\n" +
+                $"Soyad: {surname}\r\n" +
+                $"Departman: {department}\r\n" +
+                $"Proje yöneticisi olarak çalışıyorum. ");
+        }
+    }
+    public class YazilimGelistirici : Calisan // Yazılım geliştirici sınıfı, 'Calisan' sınıfından türetiliyor.
+    {
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Ad: {name}\r\n" +
+                $"Soyad: {surname}\r\n" +
+                $"Departman: {department}\r\n" +
+                $"Yazılım Geliştiricisi olarak çalışıyorum. ");
+        }
+    }
+    public class SatisTemsilcisi : Calisan // Satış temsilcisi sınıfı, 'Calisan' sınıfından türetiliyor.
+    {
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Ad: {name}\r\n" +
+                $"Soyad: {surname}\r\n" +
+                $"Departman: {department}\r\n" +
+                $"Satış Temsilcisi olarak çalışıyorum. ");
+        }
+    }
+}
---
+++ week-4/Abstraction/Abstraction/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Proje yöneticisi örneği oluşturuluyor
+            ProjeYoneticisi projeYoneticisi = new ProjeYoneticisi();
+            projeYoneticisi.GetInfo();  // Çıktı: Proje yöneticisi olarak çalışıyorum.
+
+            // Yazılım geliştirici örneği oluşturuluyor
+            Calisan yazilimGelistirici = new YazilimGelistirici();
+            yazilimGelistirici.GetInfo();  // Çıktı: Yazılım geliştirici olarak çalışıyorum.
+
+            // Satış temsilcisi örneği oluşturuluyor
+            Calisan satisTemsilcisi = new SatisTemsilcisi();
+            satisTemsilcisi.GetInfo();  // Çıktı: Satış temsilcisi olarak çalışıyorum.
+
+            Console.ReadLine();
+        }
+    }
+}
