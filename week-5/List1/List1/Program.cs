c50e1b581188d33d4f5ef16cb9025bf393133007
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-10-28 23:10:07 +0300

    Proje dosyaları oluşturuldu.

---
+++ week-5/List1/List1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace List1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> ziyaretci = new List<string>(); // 'ziyaretci' isimli bir string listesi oluşturuyoruz
+
+            ziyaretci.Add("Bülent Ersoy"); // Listeye ziyaretçi isimlerini ekliyoruz
+            ziyaretci.Add("Ajda Pekkan");
+            ziyaretci.Add("Ebru Gündeş");
+            ziyaretci.Add("Hadise");
+            ziyaretci.Add("Hande Yener");
+            ziyaretci.Add("Tarkan");
+            ziyaretci.Add("Funda Arar");
+            ziyaretci.Add("Demet Akalın");
+
+            int i = 1; // Sayacımızı başlatıyoruz, böylece her isim sırasıyla numaralandırılacak
+            foreach (string item in ziyaretci) // foreach döngüsü ile her bir ismi ve sırasını ekrana yazdırıyoruz
+            {
+                Console.WriteLine($"{i} - {item}");
+                i++; // Her adımda sayacı bir artırıyoruz
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
