0dc1704b9636b1a5c74715e2b0a37bbe24233f3c
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-10-28 23:56:49 +0300

    Proje dosyaları oluşturuldu.

---
+++ week-5/List2/List2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace List2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> kahve = new List<string>();  // Kahve isimlerini saklamak için bir liste oluşturuluyor
+            for (int i = 1; i < 6; i++) // Kullanıcıdan 5 kahve ismi alınacak
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"Kahve {i}: "); // Kullanıcıdan kahve adı istenirken 'Kahve {i}: ' ifadesi kırmızı renkte yazılıyor
+                Console.ForegroundColor = ConsoleColor.Green; // Kullanıcının girdiği kahve ismi yeşil renkte görüntüleniyor
+                kahve.Add(Console.ReadLine()); // Kullanıcının girdiği kahve adı listeye ekleniyor
+            }
+            Console.ForegroundColor = ConsoleColor.White; // Renk varsayılana döndürülüyor ve başlık yazdırılıyor
+            Console.WriteLine("\r\nGirilen Kahve İsimleri\r\n");
+            foreach (string item in kahve) // Tüm kahve isimleri listeden okunarak ekrana yazdırılıyor
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
