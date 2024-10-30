fa5388963e8ef8407f69d280e81f8d4b1ed2f8a2
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-10-30 21:05:19 +0300

    Proje dosyaları oluşturuldu.

---
+++ week-5/Try-Catch/Try-Catch/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Try_Catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // sayiMi değişkeni ile kullanıcıdan geçerli bir sayı alınıp alınmadığını kontrol ediyoruz
+            bool sayiMi = false;
+            while (!sayiMi) // sayiMi 'false' olduğu sürece döngü çalışmaya devam edecek
+            {
+                try
+                {
+                    Console.Write("Bir sayı giriniz: "); // Kullanıcıdan bir sayı girmesini istiyoruz
+                    string input = Console.ReadLine(); // Girdiyi 'string' olarak alıyoruz
+                    int sayi = Convert.ToInt32(input); // Girdiyi 'int' tipine dönüştürüyoruz
+
+                    sayiMi = true;  // Eğer dönüşüm başarılı olursa, geçerli bir sayı almış oluyoruz ve döngü sonlanıyor
+
+                    Console.WriteLine("Girdiğiniz sayının karesi: " + (sayi * sayi)); // Girilen sayının karesini ekrana yazdırıyoruz.
+                }
+                catch (Exception)
+                {
+                    // Eğer dönüştürme sırasında bir hata oluşursa, kullanıcıya geçersiz giriş olduğunu söylüyoruz
+                    Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+                }
+            }
+        }
+    }
+}
