3bba7cc6340ae10d6a8251e745b0891bd617acae
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-11-08 23:01:06 +0300

    Ödev dosyası oluşturuldu.

---
+++ week-6/Odev09/SqlQuery_1.sql
@@ -0,0 +1,9 @@
+﻿--S01 - City tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT city.city, country.country FROM country
+INNER JOIN city ON country.country_id = city.country_id; 
+--S02 - Customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM payment
+INNER JOIN customer ON payment.customer_id = customer.customer_id;
+--S03 - Customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT payment.rental_id, customer.first_name, customer.last_name FROM payment
+INNER JOIN customer ON payment.customer_id = customer.customer_id;
