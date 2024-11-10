dd1099818fc0d105e0f86dc4d4d913b1b3c2df6f
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-11-10 14:41:51 +0300

    Ödev dosyası oluşturuldu.

---
+++ week-6/Odev10/SqlQuery_1.sql
@@ -0,0 +1,9 @@
+﻿--S01 - City tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+SELECT city, country FROM city
+LEFT JOIN country ON city.country_id = country.country_id;
+--S02 - Customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+SELECT first_name, last_name, payment_id FROM customer
+RIGHT JOIN payment ON customer.customer_id=payment.customer_id;
+--S03 - Customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+SELECT rental_id, first_name, last_name FROM customer
+FULL JOIN rental ON customer.customer_id = rental.customer_id;
