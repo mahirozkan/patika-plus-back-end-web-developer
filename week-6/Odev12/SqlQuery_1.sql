c361102627701b52a5a543eb4ead6f4cee0ad598
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-11-11 22:00:01 +0300

    Ödev dosyaları oluşturuldu.

---
+++ week-6/Odev12/SqlQuery_1.sql
@@ -0,0 +1,12 @@
+﻿--S01 - Film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+SELECT COUNT(*) FROM film
+WHERE length > (SELECT AVG(length) FROM film);
+--S02 - Film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+SELECT COUNT(*) FROM film
+WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+--S03 - Film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+SELECT * FROM film
+WHERE rental_rate = (SELECT MIN(rental_rate) FROM film) AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+--S04 - Payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+SELECT customer_id, COUNT(*) FROM payment
+GROUP BY customer_id;
