5ba959267e844d3be81574c65cab2d5d80c8c14b
Mahir Ozkan <0xmahirozkan@gmail.com>
2024-11-15 19:22:27 +0300

    week-7 LinqJoin ödev dosyaları oluşturuldu.

---
+++ week-7/LinqJoin/LinqJoin/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace LinqJoin
+{
+    public class Authors // Yazar sınıfı
+    { 
+        public int AuthorID { get; set; }
+        public string Name { get; set; }
+    }
+    public class Books // Kitap sınıfı
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public int AuthorID { get; set; }
+    }
+}
---
+++ week-7/LinqJoin/LinqJoin/Program.cs
@@ -0,0 +1,53 @@
+﻿using LinqJoin;
+using System.Threading.Channels;
+
+namespace LinqJoin
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Yazarları tutan bir liste oluşturuluyor
+            List<Authors> authors = new List<Authors>();
+            authors.Add(new Authors { AuthorID = 1, Name = "Orhan Pamuk" });
+            authors.Add(new Authors { AuthorID = 2, Name = "Elif Şafak" });
+            authors.Add(new Authors { AuthorID = 3, Name = "Ahmet Ümit" });
+
+            // Kitapları tutan bir liste oluşturuluyor
+            List<Books> books = new List<Books>();
+            books.Add(new Books { BookID = 1, Title = "Kar", AuthorID = 1 });
+            books.Add(new Books { BookID = 2, Title = "İstanbul", AuthorID = 1 });
+            books.Add(new Books { BookID = 3, Title = "10 Minutes 38 Seconds In The Strange World", AuthorID = 2 });
+            books.Add(new Books { BookID = 4, Title = "Beyoğlu Rapsodisi", AuthorID = 3 });
+
+            //var authorsList = authors.Join(books, // `authors` listesi ile `books` listesi birleştiriliyor
+            //                                author => author.AuthorID, // `author` (yazar) nesnesinin AuthorID özelliği, join işlemi için kullanılıyor
+            //                                book => book.AuthorID, book => book.AuthorID, // `book` (kitap) nesnesinin AuthorID özelliği ile eşleştiriliyor
+            //                                (author, book) => new // Her iki öğe (yazar ve kitap) birleştirilip yeni bir anonim tip oluşturuluyor
+            //                                {
+            //                                    BookID = book.BookID, // Kitap ID'si
+            //                                    Title = book.Title, // Kitap başlığı
+            //                                    Name = author.Name, // Yazar adı
+            //                                    AuthorID = author.AuthorID // Yazar ID'si
+            //                                }
+            //                                );
+
+            // LINQ kullanarak Authors ve Books listelerini AuthorID üzerinden birleştiriyoruz
+            var authorList = from author in authors
+                             join book in books on author.AuthorID equals book.AuthorID
+                             select new
+                             {
+                                 BookID = book.BookID, // Kitap ID'si
+                                 Title = book.Title, // Kitap başlığı
+                                 Name = author.Name, // Yazar adı
+                                 AuthorID = author.AuthorID // Yazar ID'si
+                             };
+
+            foreach (var book in authorList) // Birleştirilen verileri ekrana yazdırıyoruz
+            {
+                Console.WriteLine($"Kitap ID: {book.BookID,-3}  ||  Kitap Adı: {book.Title,-45}  ||  Yazar Adı: {book.Name,-15}  ||  Yazar ID: {book.AuthorID}");
+            }
+        }
+    }
+}
+
